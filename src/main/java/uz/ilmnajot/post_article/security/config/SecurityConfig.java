2a90d60c68f80a9768e260cb599356c1a2381d71
Elbekjon <96689605+ilmnajot@users.noreply.github.com>
2024-12-08 16:07:43 +0500

    Some changes!

--- src/main/java/uz/ilmnajot/post_article/security/config/SecurityConfig.java
+++ src/main/java/uz/ilmnajot/post_article/security/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest -> authRequest
                         .requestMatchers(getWhitelist()).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
